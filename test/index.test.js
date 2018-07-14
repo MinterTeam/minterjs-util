890a78e5a23a8b911947d9d50765480881e2aad8
shrpne <shrpne@gmail.com>
2018-07-14 03:42:40 +0300

    improve coverage

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,20 @@
 import {Buffer} from 'safe-buffer';
 import * as minterUtil from '../src/index';
 
+describe('.mPrefixToHex()', () => {
+    test('should work', () => {
+        expect(minterUtil.mPrefixToHex('Mx2f015c60e0be116b1f0cd534704db9c92118fb6a')).toBe('0x2f015c60e0be116b1f0cd534704db9c92118fb6a');
+        expect(minterUtil.mPrefixToHex('Mh4eacb2e4bcc0514d5526895f4f699acf543081d2')).toBe('0x4eacb2e4bcc0514d5526895f4f699acf543081d2');
+    });
+});
+
+describe('.mPrefixStrip()', () => {
+    test('should work', () => {
+        expect(minterUtil.mPrefixStrip('Mx2f015c60e0be116b1f0cd534704db9c92118fb6a')).toBe('2f015c60e0be116b1f0cd534704db9c92118fb6a');
+        expect(minterUtil.mPrefixStrip('Mh4eacb2e4bcc0514d5526895f4f699acf543081d2')).toBe('4eacb2e4bcc0514d5526895f4f699acf543081d2');
+    });
+});
+
 describe('.isValidPublic()', () => {
     test('should fail on too short input', () => {
         let pubKey = '3a443d8381a6798a70c6ff9304bdc8cb0163c23211d11628fae52ef9e0dca11a001cf066d56a8156fc201cd5df8a36ef694eecd258903fca7086c1fae744';
@@ -136,12 +150,27 @@ describe('.mToBuffer()', () => {
         const buf = Buffer.from('7633980c000139dd3bd24a3f54e06474fa941e16', 'hex');
         expect(address).toEqual(buf);
     });
+    test('should throw with not string', () => {
+        expect(() => {
+            minterUtil.mToBuffer(null);
+        }).toThrow();
+    });
+    test('should throw with wrong prefix', () => {
+        expect(() => {
+            minterUtil.mToBuffer('0x28c07651a5e9ee18d746aa322967afb0f6af6f1d614e1c0226e40d392f410544');
+        }).toThrow();
+    });
 });
 
 describe('.toBuffer()', () => {
     test('should work with null', () => {
         const address = minterUtil.toBuffer(null).toString('hex');
         const buf = new Buffer([]).toString('hex');
+        expect(address).toEqual(buf);
+    });
+    test('should work with address', () => {
+        const address = minterUtil.toBuffer('Mx2f015c60e0be116b1f0cd534704db9c92118fb6a').toString('hex');
+        const buf = '2f015c60e0be116b1f0cd534704db9c92118fb6a';
         expect(address).toEqual(buf);
     });
 });
